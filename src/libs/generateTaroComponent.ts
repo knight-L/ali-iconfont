65ec1b1c0f33e2c5ecf531d8ea371229535cef92
knight <941122922@qq.com>
2024-11-21 15:13:06 +0800

    fix: 修复图标颜色失效

--- src/libs/generateTaroComponent.ts
+++ src/libs/generateTaroComponent.ts
@@ -2,13 +2,12 @@
  * @Author: Knight
  * @Date: 2024-11-21 10:09:21
  * @LastEditors: Knight
- * @LastEditTime: 2024-11-21 14:45:45
+ * @LastEditTime: 2024-11-21 15:11:52
  */
 import fs from "fs";
 import path from "path";
 import colors from "colors";
 import { Config } from "./getConfig";
-import { generateCase } from "./utils";
 import {
   replaceComponentName,
   replaceExports,
@@ -17,6 +16,7 @@ import {
 import { getTemplate } from "./getTemplate";
 import { XmlData } from "../commands/createIcon";
 import { camelCase, upperFirst } from "lodash";
+import { whitespace } from "./whitespace";
 
 export const generateTaroComponent = (data: XmlData, config: Config) => {
   const names: string[] = [];
@@ -65,4 +65,69 @@ export const generateTaroComponent = (data: XmlData, config: Config) => {
       config.save_dir
     )}\n`
   );
+};
+
+const ATTRIBUTE_FILL_MAP = ["path"];
+
+export const generateCase = (
+  data: XmlData["svg"]["symbol"][number],
+  baseIdent: number
+) => {
+  let template = `<svg viewBox='${data.$.viewBox}' xmlns='http://www.w3.org/2000/svg' width='1em' height='1em'>`;
+
+  for (const domName of Object.keys(data)) {
+    if (domName === "$") {
+      continue;
+    }
+
+    const counter = {
+      colorIndex: 0,
+      baseIdent,
+    };
+
+    if (data[domName].$) {
+      template += `<${domName}${addAttribute(
+        domName,
+        data[domName],
+        counter
+      )} />`;
+    } else if (Array.isArray(data[domName])) {
+      data[domName].forEach((sub) => {
+        template += `<${domName}${addAttribute(domName, sub, counter)} />`;
+      });
+    }
+  }
+
+  template += `</svg>`;
+
+  return template.replace(/<|>/g, (matched) => encodeURIComponent(matched));
+};
+
+export const addAttribute = (
+  domName: string,
+  sub: XmlData["svg"]["symbol"][number]["path"][number],
+  counter: { colorIndex: number; baseIdent: number }
+) => {
+  let template = "";
+
+  if (sub && sub.$) {
+    if (ATTRIBUTE_FILL_MAP.includes(domName)) {
+      sub.$.fill = sub.$.fill || "currentColor";
+    }
+
+    for (let attributeName in sub.$) {
+      if (attributeName === "fill") {
+        template += `${whitespace(counter.baseIdent + 4)}${camelCase(
+          attributeName
+        )}='${sub.$[attributeName]}'`;
+        counter.colorIndex += 1;
+      } else {
+        template += `${whitespace(counter.baseIdent + 4)}${camelCase(
+          attributeName
+        )}="${sub.$[attributeName]}"`;
+      }
+    }
+  }
+
+  return template;
 };
